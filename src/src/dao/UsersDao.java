e1fa4defffde1793870edf572ee477401151c942
dojo2023-sasahara <130331138+dojo2023-sasahara@users.noreply.github.com>
2023-06-14 17:08:30 +0900

    dao.model追加

--- src/src/dao/UsersDao.java
+++ src/src/dao/UsersDao.java
@@ -3,18 +3,176 @@
  */
 package dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import model.User;
+
 /**
  * @author user
  *
  */
 public class UsersDao {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
+		// 引数paramで検索項目を指定し、検索結果のリストを返す
+		public List<User> select(User param) {
+			Connection conn = null;
+			List<User> cardList = new ArrayList<User>();
+
+			try {
+				// JDBCドライバを読み込む
+				Class.forName("org.h2.Driver");
+
+				// データベースに接続する
+				conn = DriverManager.getConnection("jdbc:h2:file:C:/pleiades/workspace/data/C4","sa","");
+
+				// SQL文を準備する
+				String sql = "select  USER_ID,USER_PW,USER_HOMEID,USER_GENDERID,USER_PTEMPERTUREID from USERS WHERE ID LIKE ? ORDER BY ID";
+				PreparedStatement pStmt = conn.prepareStatement(sql);
+
+				// SQL文を完成させる
+				if (param.getID() != 0) {
+					pStmt.setString(1, "%" + param.getID() + "%");
+				}
+
+
+
+
+
+
+
+
+
+				// SQL文を実行し、結果表を取得する
+				ResultSet rs = pStmt.executeQuery();
+
+				// 結果表をコレクションにコピーする
+				while (rs.next()) {
+					User card = new User(
+					rs.getInt("ID"),
+					rs.getString("USER_ID"),
+					rs.getString("USER_PW"),
+					rs.getInt("USER_HOMEID"),
+					rs.getInt("USER_GENDERID"),
+					rs.getInt("USER_PTEMPERETUREID")
+					);
+					cardList.add(card);
+				}
+			}
+			catch (SQLException e) {
+				e.printStackTrace();
+				cardList = null;
+			}
+			catch (ClassNotFoundException e) {
+				e.printStackTrace();
+				cardList = null;
+			}
+			finally {
+				// データベースを切断
+				if (conn != null) {
+					try {
+						conn.close();
+					}
+					catch (SQLException e) {
+						e.printStackTrace();
+						cardList = null;
+					}
+				}
+			}
+
+			// 結果を返す
+			return cardList;
+		}
+
+
+		// 引数profileで指定されたレコードを更新し、成功したらtrueを返す
+		public boolean update(User profile) {
+			Connection conn = null;
+			boolean result = false;
+
+			try {
+				// JDBCドライバを読み込む
+				Class.forName("org.h2.Driver");
+
+				// データベースに接続する
+				conn = DriverManager.getConnection("jdbc:h2:file:C:/pleiades/workspace/data/C4","sa","");
+
+				// SQL文を準備する
+				String sql = "update USERS set USER_ID=?,USER_PW=?,USER_HOMEID=?,USER_GENDERID=?,USER_PTEMPERTUREID=?  where ID=? ";
+//
+				PreparedStatement pStmt = conn.prepareStatement(sql);
+
+				// SQL文を完成させる
+				if (profile.getUSER_ID() != null && !profile.getUSER_ID().equals("")) {
+					pStmt.setString(1, profile.getUSER_ID());
+				}
+				else {
+					pStmt.setString(1, null);
+				}
+				if (profile.getUSER_PW() != null && !profile.getUSER_PW().equals("")) {
+					pStmt.setString(2, profile.getUSER_PW());
+				}
+				else {
+					pStmt.setString(2, null);
+				}
+				if (profile.getUSER_HOMEID() != 0 ) {
+					pStmt.setInt(3, profile.getUSER_HOMEID());
+				}
+				else {
+					pStmt.setString(3, null);
+				}
+
+				if (profile.getUSER_GENDERID() != 0 ) {
+					pStmt.setInt(4, profile.getUSER_GENDERID());
+				}
+				else {
+					pStmt.setString(4, null);
+				}
+
+				if (profile.getUSER_PTEMPERTUREID() != 0 ) {
+					pStmt.setInt(5, profile.getUSER_PTEMPERTUREID());
+				}
+				else {
+					pStmt.setString(5, null);
+				}
+
+
+				pStmt.setInt(6, profile.getID());
+
+				// SQL文を実行する
+				if (pStmt.executeUpdate() == 1) {
+					result = true;
+				}
+			}
+			catch (SQLException e) {
+				e.printStackTrace();
+			}
+			catch (ClassNotFoundException e) {
+				e.printStackTrace();
+			}
+			finally {
+				// データベースを切断
+				if (conn != null) {
+					try {
+						conn.close();
+					}
+					catch (SQLException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+
+			// 結果を返す
+			return result;
+		}
+
+
+
+
 
 	}
-
-}
--- src/src/model/User.java
+++ src/src/model/User.java
@@ -1,10 +1,78 @@
 package model;
+import java.io.Serializable;
 
-public class User {
 
-	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
+public class User implements Serializable {
+	private int ID;
+	private String USER_ID;
+	private String USER_PW;
+	private int USER_HOMEID;
+	private int USER_GENDERID;
+	private int USER_PTEMPERTUREID;
+//引数アリのコンストラクタの設定
+	public User(String USER_ID,String USER_PW,int USER_HOMEID,int USER_GENDERID,int USER_PTEMPERTUREID){
+		this.USER_ID = USER_ID;
+		this.USER_PW = USER_PW;
+		this.USER_HOMEID = USER_HOMEID;
+        this.USER_GENDERID = USER_GENDERID;
+        this.USER_PTEMPERTUREID = USER_PTEMPERTUREID;
 
 	}
+
+	public User(int ID,String USER_ID,String USER_PW,int USER_HOMEID,int USER_GENDERID,int USER_PTEMPERTUREID){
+		this.ID = ID;
+		this.USER_ID = USER_ID;
+		this.USER_PW = USER_PW;
+		this.USER_HOMEID = USER_HOMEID;
+        this.USER_GENDERID = USER_GENDERID;
+        this.USER_PTEMPERTUREID = USER_PTEMPERTUREID;
+        }
+
+//引数なしのコンストラクタの設定
+	public User(){
+		this.USER_ID = "";
+		this.USER_PW = "";
+		this.USER_HOMEID = 0;
+        this.USER_GENDERID = 0;
+        this.USER_PTEMPERTUREID = 0;
+
+	}
+	public int getID() {
+		return ID;
+	}
+
+
+	public String getUSER_ID() {
+		return USER_ID;
+	}
+	public void setUSER_ID(String USER_ID) {
+		this.USER_ID = USER_ID;
+	}
+	public String getUSER_PW() {
+		return USER_PW;
+	}
+	public void setUSER_PW(String USER_PW) {
+		this.USER_PW = USER_PW;
+	}
+	public int getUSER_HOMEID() {
+		return USER_HOMEID;
+	}
+	public void setUSER_HOMEID(int USER_HOMEID) {
+		this.USER_HOMEID = USER_HOMEID;
+	}
+	public int getUSER_GENDERID() {
+		return USER_GENDERID;
+	}
+	public void setUSER_GENDERID(int USER_GENDERID) {
+		this.USER_GENDERID = USER_GENDERID;
+	}
+	public int getUSER_PTEMPERTUREID() {
+		return USER_PTEMPERTUREID;
+	}
+	public void setUSER_PTEMPERTUREID(int USER_PTEMPERTUREID) {
+		this.USER_PTEMPERTUREID = USER_PTEMPERTUREID;
+	}
+
+
 
 }

f57f4b1a4bb79452c1b9d08217f1c18594666c41
dojo2023-matsukura <130334630+dojo2023-matsukura@users.noreply.github.com>
2023-06-15 13:25:49 +0900

    Daomodel

--- src/src/dao/ClothesDao.java
+++ src/src/dao/ClothesDao.java
@@ -1,5 +1,78 @@
 package dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import model.Cloth;
+
 public class ClothesDao {
+	// 引数paramで検索項目を指定し、検索結果のリストを返す
+			public List<Cloth> select(Cloth param) {
+				Connection conn = null;
+				List<Cloth> cardList = new ArrayList<Cloth>();
+//tryの上まで１２:00
+
+				try {
+					// JDBCドライバを読み込む,
+					Class.forName("org.h2.Driver");
+
+					// データベースに接続する
+					conn = DriverManager.getConnection("jdbc:h2:file:C:/pleiades/workspace/data/C4","sa","");
+
+					// SQL文を準備する
+					String sql = "select ID, USER_ID, CLO_IMAGES ,SMALL_CATEGORYID from CLOTHES WHERE ID LIKE ? ORDER BY ID";
+					PreparedStatement pStmt = conn.prepareStatement(sql);
+
+					// SQL文を完成させる
+					if (param.getID() != 0) {
+						pStmt.setString(1, "%" + param.getID() + "%");
+					}
+
+
+
+					// SQL文を実行し、結果表を取得する
+					ResultSet rs = pStmt.executeQuery();
+
+					// 結果表をコレクションにコピーする
+					while (rs.next()) {
+						Cloth card = new Cloth(
+						rs.getInt("ID"),
+						rs.getString("USER_ID"),
+						rs.getInt("SMALL_CATEGORYID"),
+						rs.getString("CLO_IMAGES")
+						);
+						cardList.add(card);
+					}
+				}
+				catch (SQLException e) {
+					e.printStackTrace();
+					cardList = null;
+				}
+				catch (ClassNotFoundException e) {
+					e.printStackTrace();
+					cardList = null;
+				}
+				finally {
+					// データベースを切断
+					if (conn != null) {
+						try {
+							conn.close();
+						}
+						catch (SQLException e) {
+							e.printStackTrace();
+							cardList = null;
+						}
+					}
+				}
+
+				// 結果を返す
+				return cardList;
+			}
+
 
 }

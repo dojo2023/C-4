3193195b4d9c4b9ae58ea1487f5b3cfc08ff7848
dojo2023-sasahara <130331138+dojo2023-sasahara@users.noreply.github.com>
2023-06-15 10:55:40 +0900

    dao追加

---
+++ src/src/model/Weather.java
@@ -0,0 +1,60 @@
+/**
+ *
+ */
+package model;
+
+import java.io.Serializable;
+
+/**
+ * @author user
+ *
+ */
+public class Weather implements Serializable{
+		private int ID;
+		private int DAY_WEATHERCODE;
+		private String WEATHER_IMAGE;
+		private String WEATHER_EXPRESSION;
+
+		public Weather(int ID){
+			this.ID = ID;
+			this.DAY_WEATHERCODE = 100;
+			this.WEATHER_IMAGE ="" ;
+	        this.WEATHER_EXPRESSION = "";
+            }
+
+		public int getID() {
+			return ID;
+		}
+
+		public void setID(int ID) {
+			this.ID = ID;
+		}
+
+		public int getDAY_WEATHERCODE() {
+			return DAY_WEATHERCODE;
+		}
+
+		public void setDAY_WEATHERCODE(int DAY_WEATHERCODE) {
+			this.DAY_WEATHERCODE = DAY_WEATHERCODE;
+		}
+
+		public String getWEATHER_IMAGE() {
+			return WEATHER_IMAGE;
+		}
+
+		public void setWEATHER_IMAGE(String WEATHER_IMAGE) {
+			this.WEATHER_IMAGE = WEATHER_IMAGE;
+		}
+
+		public String getWEATHER_EXPRESSION() {
+			return WEATHER_EXPRESSION;
+		}
+
+		public void setWEATHER_EXPRESSION(String WEATHER_EXPRESSION) {
+			this.WEATHER_EXPRESSION = WEATHER_EXPRESSION;
+		}
+
+
+
+
+}

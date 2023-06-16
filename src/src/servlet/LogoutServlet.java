772669bbc143c78399c01f0a8447e1e5ee2ec35a
dojo2023-sasahara <130331138+dojo2023-sasahara@users.noreply.github.com>
2023-06-16 17:20:13 +0900

    logout

--- src/src/servlet/LogoutServlet.java
+++ src/src/servlet/LogoutServlet.java
@@ -8,11 +8,19 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Servlet implementation class logout
+ * Servlet implementation class LogoutServlet
  */
-@WebServlet("/logout")
-public class logout extends HttpServlet {
+@WebServlet("/LogoutServlet")
+public class LogoutServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LogoutServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
